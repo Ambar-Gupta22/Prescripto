2eb493c020332e398c9d5fd86a2758fdbe1ec65f
Ambar Gupta <gvansh2211@gmail.com>
2025-01-28 00:09:51 +0530

    fifth-commit

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5174 },
-  build: {
-    target: 'esnext', // Ensures compatibility with modern browsers
-    outDir: 'dist',   // Output directory for build files
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Let Rollup handle chunk splitting automatically
-      },
-    },
+  server: {
+    port: 5174
   },
+  build: {
+    target: 'esnext' // You can use 'es2020' for broader compatibility
+  }
 });
